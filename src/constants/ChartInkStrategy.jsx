380c2bd86e7898022fc06146d71d91bcf9fde060
imshahbaz21 <imshahbaz21@gmail.com>
2024-01-12 15:09:27 +0530

    hammer

--- src/constants/ChartInkStrategy.jsx
+++ src/constants/ChartInkStrategy.jsx
@@ -1,12 +1,17 @@
 export const chartink_strategy2 = [
   {
-    name: 'Triple EMA',
-    link: 'https://chartink.com/screener/5813ema-4',
+    name: '3:30 Formula',
+    link: 'https://chartink.com/screener/3-30-2138',
     display: true,
   },
   {
-    name: '3:30 Formula',
-    link: 'https://chartink.com/screener/3-30-2138',
+    name: 'Hammer',
+    link: 'https://chartink.com/screener/hammer-16123355',
+    display: true,
+  },
+  {
+    name: 'Triple EMA',
+    link: 'https://chartink.com/screener/5813ema-4',
     display: true,
   },
   {
